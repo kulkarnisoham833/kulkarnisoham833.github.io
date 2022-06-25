2bfc3e367a303869cbe455931230c58927eb9a5f
Soham Kulkarni <63484051+kulkarnisoham833@users.noreply.github.com>
2022-06-25 12:03:31 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function login(fname, lname) {
-    document.getElementById('fullname').innerHTML = fname + ' ' + lname;
-    document.getElementById('fullname').style.display = 'block';
-    event.preventDefault();
-}
